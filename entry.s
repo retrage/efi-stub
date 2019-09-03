385b916949300c0babd478d0ffa4c431020b8352
Akira Moroo <retrage01@gmail.com>
2019-09-03 20:04:47 +0900

    Fix entry assembly
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- entry.s
+++ entry.s
@@ -5,7 +5,7 @@
 .text
 
 _entry:
-  movq %rdi, %rcx /* EFI_HANDLE */
-  movq %rsi, %rdx /* EFI_SYSTEM_TABLE* */
+  movq %rcx, %rdi /* EFI_HANDLE */
+  movq %rdx, %rsi /* EFI_SYSTEM_TABLE* */
   call efi_main
   ret           /* return value in rax */
